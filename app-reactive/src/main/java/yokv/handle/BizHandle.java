ac291f5d0bf17f93d0a8df4bbda30bef7deef384
yokv <ez4tzl@hotmail.com>
2018-05-27 15:25:51 +0800

    handle

--- app-reactive/src/main/java/yokv/handle/BizHandle.java
+++ app-reactive/src/main/java/yokv/handle/BizHandle.java
@@ -1,10 +1,10 @@
 package yokv.handle;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 import java.time.LocalDateTime;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -17,35 +17,46 @@ import yokv.service.BizService;
 @Component
 public class BizHandle {
 
-  @Autowired
+  private final static Logger LOGGER = LoggerFactory.getLogger(BizHandle.class);
+
+
   private ReactiveRedisTemplate<Object, Object> reactiveRedisTemplate;
-  @Autowired
+  private StringRedisTemplate stringRedisTemplate;
   private BizService bizService;
 
+  public BizHandle(
+      ReactiveRedisTemplate<Object, Object> reactiveRedisTemplate,
+      StringRedisTemplate stringRedisTemplate, BizService bizService) {
+    this.reactiveRedisTemplate = reactiveRedisTemplate;
+    this.stringRedisTemplate = stringRedisTemplate;
+    this.bizService = bizService;
+  }
 
   public Mono<ServerResponse> start(ServerRequest request) {
-
-    reactiveRedisTemplate.opsForList().rightPop("okhttp1.1" + LocalDateTime.now());
-
-    return ServerResponse.ok().contentType(APPLICATION_JSON)
-        .body(reactiveRedisTemplate.opsForValue().get("key"), Object.class);
+    return common(request, "okhttp1.1", bizService.method1());
   }
 
   public Mono<ServerResponse> method1(ServerRequest request) {
-    reactiveRedisTemplate.opsForList().rightPop("httpclient1.1" + LocalDateTime.now());
-
-    return ServerResponse.ok().contentType(APPLICATION_JSON)
-        .body(reactiveRedisTemplate.opsForValue().get("key"), Object.class);
+    return common(request, "httpclient1.1", bizService.method2());
   }
 
   public Mono<ServerResponse> method2(ServerRequest request) {
-    reactiveRedisTemplate.opsForList().rightPop("grpc" + LocalDateTime.now());
-    return ServerResponse.ok().contentType(APPLICATION_JSON)
-        .body(reactiveRedisTemplate.opsForValue().get("key"), Object.class);
+    return common(request, "grpc", bizService.method3());
   }
 
   public Mono<ServerResponse> method3(ServerRequest request) {
-    return ServerResponse.ok().contentType(APPLICATION_JSON)
-        .body(reactiveRedisTemplate.opsForValue().get("key"), Object.class);
+    return common(request, "end", "end");
   }
+
+  private Mono<ServerResponse> common(ServerRequest request, String value, String remote) {
+    LOGGER.info("path {} attributes {} cookies {} formData {} headers {} queryParams {} method {}",
+        request.path(), request.attributes(),
+        request.cookies(), request.formData(),
+        request.headers(), request.queryParams(), request.method());
+    reactiveRedisTemplate.opsForList().rightPush("list", value + LocalDateTime.now());
+    String result =
+        stringRedisTemplate.boundValueOps("increment").increment(1L) + "," + remote;
+    return ServerResponse.ok().body(Mono.just(result), String.class);
+  }
+
 }
