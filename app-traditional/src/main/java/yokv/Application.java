99039f7743b8141798044910a43dfef3be3e20ff
yokv <ez4tzl@hotmail.com>
2018-05-27 19:18:03 +0800

    okhttp

--- app-traditional/src/main/java/yokv/Application.java
+++ app-traditional/src/main/java/yokv/Application.java
@@ -1,6 +1,7 @@
 package yokv;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableAutoConfiguration
 public class Application {
 
   public static void main(String[] args) {
